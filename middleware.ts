e69cf06ff1121e1ad594890260166abba713fd4d
AndyMagwayer <andymagwayer@gmial.com>
2023-08-05 11:20:29 +0500

    feat: completed sign in and sing up

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,16 @@
-import { AuthMethodsConfiguration } from "@clerk/nextjs";
+// Resource: https://clerk.com/docs/nextjs/middleware#auth-middleware
+// Copy the middleware code as it is from the above resource
+
+import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ['/', '/api/webhook/clerk'],
-    ignoredRoutes: ['.api/webhook/clerk']
+  // An array of public routes that don't require authentication.
+  publicRoutes: ["/api/webhook/clerk"],
+
+  // An array of routes to be ignored by the authentication middleware.
+  ignoredRoutes: ["/api/webhook/clerk"],
 });
 
 export const config = {
-    matcher: ["/((?!.*|_next)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
