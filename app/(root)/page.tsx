0c68ac2019d6dbe155220726a7b3ae9324f7c8e8
AndyMagwayer <andymagwayer@gmial.com>
2023-08-05 11:15:32 +0500

    feat: added registration navabar fully but it should to fixed by position (11:14)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
+//app/page.tsx
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Threads</h1>
-
-    </main>
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   )
 }
---
+++ middleware.ts
@@ -0,0 +1,10 @@
+import { AuthMethodsConfiguration } from "@clerk/nextjs";
+
+export default authMiddleware({
+    publicRoutes: ['/', '/api/webhook/clerk'],
+    ignoredRoutes: ['.api/webhook/clerk']
+});
+
+export const config = {
+    matcher: ["/((?!.*|_next)", "/", "/(api|trpc)(.*)"],
+};
