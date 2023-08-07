c00f90cb148a9c3af1d2a2e8e1d981ab8fe4a794
AndyMagwayer <andymagwayer@gmial.com>
2023-08-07 10:26:52 +0500

    fix: fixed redline problems under xomponents which were almost end

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,10 @@ import { ClerkProvider } from '@clerk/nextjs'
 import '../globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Topbar from '@/components/shared/Topbar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/RightSidebar'
+import Bottombar from '@/components/shared/Bottombar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,7 +26,7 @@ export default function RootLayout({
           <Topbar />
         
         <main>
-          <Leftsidebar />
+          <LeftSidebar />
 
           <section className='main-container'>
             <div className="w-full max-w-4xl">
@@ -32,7 +36,7 @@ export default function RootLayout({
           </section>
 
 
-          <Rightsidebar />
+          <RightSidebar />
         </main>
 
           <Bottombar />
