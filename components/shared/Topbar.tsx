9022bcddb6c78a5794577e65fbc7136188660374
AndyMagwayer <andymagwayer@gmial.com>
2023-08-07 11:01:01 +0500

    fix: should be fixed Sign out Button in navbar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,39 @@
+import { SignedIn, SignedOutButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+
+
 function Topbar() {
-    return <h1>Topbar</h1>
+  return (
+    <nav className="topbar">
+      <Link href="./" className="flex items-center gap-4">
+        <Image src="/assets/logo.svg" alt="logo" width={28}
+          height={28} />
+        <p className="text-heading3-bold 
+       text-light-1 max-xs:hidden">Threads</p>
+      </Link>
+
+
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignedOutButton>
+              <div className="flex cursor-pointer">
+                <Image
+
+                  src="/assets/logoout.svg"
+                  alt="Logout"
+                  width={24}
+                  height={24}
+
+                  />
+            </div>
+          </SignedOutButton>
+        </SignedIn>
+      </div>
+    </div>
+    </nav >
+    )
 }
 
-export default Topbar ;
+export default Topbar; 
