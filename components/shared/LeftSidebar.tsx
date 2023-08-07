ceac96131008d765c5c363634da67c4c3d43fe33
AndyMagwayer <andymagwayer@gmial.com>
2023-08-07 11:30:50 +0500

    feat: completed js part and with that created an folder with the class name like constants

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,5 +1,13 @@
 function LeftSidebar() {
-    return <h1>LeftSidebar</h1>
+    return (
+        <section className="custom-scrollbar leftsidebar">
+            <div className="flex w-full flex-1 flex-col gap-6 px-6">
+                
+
+            </div>
+
+        </section>
+    )
 }
 
 export default LeftSidebar ;
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,39 +1,41 @@
-import { SignedIn, SignedOutButton } from "@clerk/nextjs";
+import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
-
 function Topbar() {
   return (
-    <nav className="topbar">
-      <Link href="./" className="flex items-center gap-4">
-        <Image src="/assets/logo.svg" alt="logo" width={28}
-          height={28} />
-        <p className="text-heading3-bold 
-       text-light-1 max-xs:hidden">Threads</p>
+    <nav className='topbar'>
+      <Link href='/' className='flex items-center gap-4'>
+        <Image src='/logo.svg' alt='logo' width={28} height={28} />
+        <p className='text-heading3-bold text-light-1 max-xs:hidden'>Threads</p>
       </Link>
 
-
-      <div className="flex items-center gap-1">
-        <div className="block md:hidden">
+      <div className='flex items-center gap-1'>
+        <div className='block md:hidden'>
           <SignedIn>
-            <SignedOutButton>
-              <div className="flex cursor-pointer">
+            <SignOutButton>
+              <div className='flex cursor-pointer'>
                 <Image
-
-                  src="/assets/logoout.svg"
-                  alt="Logout"
+                  src='assets/logout.svg'
+                  alt='logout'
                   width={24}
                   height={24}
+                />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
 
-                  />
-            </div>
-          </SignedOutButton>
-        </SignedIn>
+        <OrganizationSwitcher
+          appearance={{
+            elements: {
+              organizationSwitcherTrigger: "py-2 px-4",
+            },
+          }}
+        />
       </div>
-    </div>
-    </nav >
-    )
+    </nav>
+  );
 }
 
-export default Topbar; 
+export default Topbar;
