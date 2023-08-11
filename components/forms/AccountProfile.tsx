1459fd7fce74b7ae8af310b69faa7ba84763c9a5
AndyMagwayer <andymagwayer@gmial.com>
2023-08-11 12:38:47 +0500

    feat: added shadecn npmx

---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+interface Props{
+    user: {
+        id: string;
+        objectId: string;
+        username: string;
+        name: string;
+        bio: string;
+        image: string;
+
+    };
+    btnTitle: string;
+    
+}
+
+const AccountProfile = ({ user, btnTitle}:
+    Props) => {
+    return(
+        <div>
+            Account Profile
+        </div>
+    )
+}
+
+export default  AccountProfile;
