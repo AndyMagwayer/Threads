a28bd0739222fdad8a2948abad6b00314e907a02
AndyMagwayer <andymagwayer@gmial.com>
2023-08-11 12:03:14 +0500

    fix: fixed the color of the workplace organzation switcher to the default theme like light (11:58)

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -16,4 +16,3 @@ function RightSidebar() {
 
 export default RightSidebar ;
 
-// jufjjfj
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,7 @@
 import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { dark } from '@clerk/themes'
 
 function Topbar() {
   return (
@@ -28,6 +29,7 @@ function Topbar() {
 
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4",
             },
